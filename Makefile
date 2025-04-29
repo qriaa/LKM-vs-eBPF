a663631099f6a5de90fff82cd8b2a84546cefe5f
qriaa <urbaniak.g11@gmail.com>
2025-04-29 17:29:57 +0200

    Add basic packet tracing using netfilter

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += hello-1.o
+obj-m += tcp-hook.o
 PWD := $(CURDIR)
 all:
 	$(MAKE) -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
---
+++ tcp-hook.c
@@ -0,0 +1,43 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+unsigned int tcp_logger(void *priv,
+                        struct sk_buff *skb,
+                        const struct nf_hook_state *state) {
+    pr_info("There is a packet!\n");
+    return NF_ACCEPT;
+}
+
+static struct nf_hook_ops tcp_logger_hook;
+
+static int __init tcp_logger_init(void) {
+    tcp_logger_hook = (struct nf_hook_ops){
+        .hook = tcp_logger,
+        .dev = NULL,
+        .priv = NULL,
+        .pf = NFPROTO_IPV4,
+        .hook_ops_type = NF_HOOK_OP_NF_TABLES,
+        .hooknum = NF_INET_PRE_ROUTING,
+        .priority = NF_IP_PRI_FIRST
+    };
+
+    nf_register_net_hook(&init_net, &tcp_logger_hook);
+    pr_info("TCP logger loaded.\n");
+    return 0;
+}
+
+static void __exit tcp_logger_exit(void) {
+    nf_unregister_net_hook(&init_net, &tcp_logger_hook);
+    pr_info("TCP logger unloaded.\n");
+}
+
+module_init(tcp_logger_init);
+module_exit(tcp_logger_exit);
+
+MODULE_AUTHOR("Author Name urbaniak.g11@gmail.com");
+MODULE_DESCRIPTION("This module performs some sample operations using netfilter");
+MODULE_LICENSE("GPL");
