be324805291b396610665302de90fd5660c3926e
qriaa <urbaniak.g11@gmail.com>
2025-05-12 20:36:42 +0200

    Print result, set packet count to 20k

--- generate-packets.sh
+++ generate-packets.sh
@@ -12,5 +12,8 @@ pg_thread $THREAD "add_device $PGDEV"
 pg_set $PGDEV "flag IPSRC_RND"
 pg_set $PGDEV "src_min 127.0.0.0"
 pg_set $PGDEV "src_max 127.0.0.255"
+pg_set $PGDEV "count 20000"
 
 pg_ctrl "start"
+
+cat /proc/net/pktgen/$PGDEV
