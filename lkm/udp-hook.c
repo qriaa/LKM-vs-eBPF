b68cb1688c3e88c7bccc9a252b733c4d8f61f125
qriaa <urbaniak.g11@gmail.com>
2025-05-21 16:38:25 +0200

    We actually track UDP packets

--- lkm/udp-hook.c
+++ lkm/udp-hook.c
@@ -3,16 +3,15 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
-#include <linux/tcp.h>
 #include <linux/inet.h>
 
-unsigned int tcp_logger(void *priv,
+unsigned int udp_logger(void *priv,
                         struct sk_buff *skb,
                         const struct nf_hook_state *state);
 
-static struct nf_hook_ops tcp_logger_hook;
+static struct nf_hook_ops udp_logger_hook;
 
-unsigned int tcp_logger(void *priv,
+unsigned int udp_logger(void *priv,
                         struct sk_buff *skb,
                         const struct nf_hook_state *state) {
     struct iphdr *ip_header;
@@ -23,16 +22,20 @@ unsigned int tcp_logger(void *priv,
 
     ip_header = ip_hdr(skb);
 
+    if (ip_header->protocol != IPPROTO_UDP) {
+        return NF_ACCEPT;
+    }
+
     __be32 src_ip = ip_header->saddr;
     __be32 dest_ip = ip_header->daddr;
 
-    pr_info("Packet: %pI4 -> %pI4\n", &src_ip, &dest_ip);
+    pr_info("[PACKET] %pI4 -> %pI4\n", &src_ip, &dest_ip);
     return NF_ACCEPT;
 }
 
-static int __init tcp_logger_init(void) {
-    tcp_logger_hook = (struct nf_hook_ops){
-        .hook = tcp_logger,
+static int __init udp_logger_init(void) {
+    udp_logger_hook = (struct nf_hook_ops){
+        .hook = udp_logger,
         .dev = NULL,
         .priv = NULL,
         .pf = NFPROTO_IPV4,
@@ -41,18 +44,18 @@ static int __init tcp_logger_init(void) {
         .priority = NF_IP_PRI_FIRST
     };
 
-    nf_register_net_hook(&init_net, &tcp_logger_hook);
-    pr_info("TCP logger loaded.\n");
+    nf_register_net_hook(&init_net, &udp_logger_hook);
+    pr_info("UDP logger loaded.\n");
     return 0;
 }
 
-static void __exit tcp_logger_exit(void) {
-    nf_unregister_net_hook(&init_net, &tcp_logger_hook);
-    pr_info("TCP logger unloaded.\n");
+static void __exit udp_logger_exit(void) {
+    nf_unregister_net_hook(&init_net, &udp_logger_hook);
+    pr_info("UDP logger unloaded.\n");
 }
 
-module_init(tcp_logger_init);
-module_exit(tcp_logger_exit);
+module_init(udp_logger_init);
+module_exit(udp_logger_exit);
 
 MODULE_AUTHOR("Author Name urbaniak.g11@gmail.com");
 MODULE_DESCRIPTION("This module performs some sample operations using netfilter");
